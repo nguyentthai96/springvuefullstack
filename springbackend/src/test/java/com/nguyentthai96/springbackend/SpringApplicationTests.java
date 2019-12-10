33be8cb61159ec42aea175546e3bd4e8f6e993f9
NguyenThanhThai <nguyentthai96@gmail.com>
2019-12-10 20:41:58 +0700

    init backend

---
+++ springbackend/src/test/java/com/nguyentthai96/springbackend/SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyentthai96.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
